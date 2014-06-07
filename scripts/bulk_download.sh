cc825c372fd1b41cfe9b75804b9d613077c9162b
Pedro Markun <pedro@markun.com.br>
2014-06-07 16:36:28 -0300

    bulk download

---
+++ scripts/bulk_download.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Cria dirs!
+mkdir -p ../raw
+mkdir -p ../raw/legis
+mkdir -p ../raw/vereador
+mkdir -p ../raw/rh
+mkdir -p ../data
+
+# Produção Legislativa
+cd ../raw
+cd legis
+encerra.txtprojetos/
+wget -N http://www2.camara.sp.gov.br/projetos/catalogo_assuntos.xml
+wget -N http://www2.camara.sp.gov.br/projetos/tipo_materia_legislativa.xml
+wget -N http://www2.camara.sp.gov.br/projetos/unidades.xml
+
+wget -N http://www2.camara.sp.gov.br/Dados_abertos/LEIA-ME.TXT
+wget -N http://www2.camara.sp.gov.br/Dados_abertos/projetos/assunto.txt
+wget -N http://www2.camara.sp.gov.br/Dados_abertos/projetos/autor.txt
+wget -N http://www2.camara.sp.gov.br/Dados_abertos/projetos/comdes.txt
+wget -N http://www2.camara.sp.gov.br/Dados_abertos/projetos/delibera.txt
+wget -N http://www2.camara.sp.gov.br/Dados_abertos/projetos/encami.txt
+wget -N http://www2.camara.sp.gov.br/Dados_abertos/projetos/encerra.txt
+wget -N http://www2.camara.sp.gov.br/Dados_abertos/projetos/tramita.txt
+wget -N http://www2.camara.sp.gov.br/Dados_abertos/projetos/prolege.txt
+wget -N http://www2.camara.sp.gov.br/Dados_abertos/projetos/prolegm.txt
+wget -N http://www2.camara.sp.gov.br/Dados_abertos/projetos/prolegpa.txt
+wget -N http://www2.camara.sp.gov.br/Dados_abertos/projetos/prolegs.txt
+wget -N http://www2.camara.sp.gov.br/Dados_abertos/projetos/prolegt.txt
+wget -N http://www2.camara.sp.gov.br/Dados_abertos/projetos/prolegp.txt
+wget -N http://www2.camara.sp.gov.br/Dados_abertos/projetos/projetos.txt
+cd ..
+
+# Vereadores
+cd vereador
+wget -N http://www2.camara.sp.gov.br/Dados_abertos/vereador/vereador.txt
+cd ..
+
+#Updated
+date > UPDATED.md
