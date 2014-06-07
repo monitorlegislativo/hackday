2ef132480fddd4b8ff5c2f0a42d099dfe30e4bef
Pedro Markun <pedro@markun.com.br>
2014-06-07 18:45:26 -0300

    server api flask

--- server/server.py
+++ server/server.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from tools import jsonify
+from flask.ext.pymongo import PyMongo
+
+
 app = Flask(__name__)
+app.config['MONGO_DBNAME'] = 'monitorlegislativo'
+#app.config["SECRET_KEY"] = "KeepThisS3cr3t"
+
+mongo = PyMongo(app)
 
 @app.route("/")
 def index():
@@ -9,7 +17,8 @@ def index():
 @app.route('/legis/<tipo>/<numero>/<ano>')
 def projeto(tipo, numero, ano):
 	pid = tipo + '-' + numero + '-' + ano
-	return pid
+	projeto = mongo.db.legis.find_one({"id": pid})
+	return jsonify(projeto)
 
 if __name__ == "__main__":
     app.run(debug=True)
