cb90e1f2888bbb495963f675fecc43b5f7797287
Pedro Markun <pedro@markun.com.br>
2014-06-09 18:49:03 -0300

    stick explicacao

--- server/static/js/main.js
+++ server/static/js/main.js
@@ -1,6 +1,8 @@
 $().ready(function() {
+	$(".explicacao").sticky({topSpacing:0});
     $(".tramitacao a").click(function(e) {
     	e.preventDefault();
+
     	var t = $(this).data("tramitacao");
     	$(".explica").hide('slow');
     	$("#T-"+t).show('slow');
