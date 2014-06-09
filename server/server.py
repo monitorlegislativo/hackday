d95058d34803e25f61ab9618965fabb5ce439cf1
Pedro Markun <pedro@markun.com.br>
2014-06-08 23:28:30 -0300

    primeira versao da busca funcionando

--- server/server.py
+++ server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect, abort
 from tools import jsonify, datefromunix
 from flask.ext.pymongo import PyMongo
 
@@ -11,17 +11,31 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    return render_template('front.html')
+	return render_template('front.html')
+
+@app.route("/busca")
+def busca():
+	termo = request.args.get('termo', '')
+	try:
+		t = termo.split()
+		numero = str(int(t[0].strip())).zfill(4) #transforma em int e depois em str de novo com 4 casas
+		ano = str(int(t[1].strip()))
+		return redirect(url_for("projeto", tipo='pl',numero=numero, ano=ano))
+	except:
+		return termo #todo
+
 
 @app.route('/legis/<tipo>/<numero>/<ano>')
 @app.route('/legis/<tipo>/<numero>/<ano>/<json>')
 def projeto(tipo, numero, ano, json=False):
 	pid = tipo + '-' + numero + '-' + ano
 	projeto = mongo.db.legis.find_one({"id": pid})
+	if not projeto:
+		abort(404)
 	if json == 'json':
 		return jsonify(projeto)
 	elif json == False:
 		return render_template('legis.html', p=projeto)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
